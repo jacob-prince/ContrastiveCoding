6f18ec88ef02d8cc78da856f87b3cc529df4894f
Jacob Prince <jacob.samuel.prince@gmail.com>
2023-01-31 21:54:23 -0500

    updated paths.py

--- PROJECT_DNFFA/paths.py
+++ PROJECT_DNFFA/paths.py
@@ -1,6 +1,9 @@
-def nsd_root():
+def nsd():
     return '/home/jovyan/work/DataLocal-w/NSD'
 
-def ffcv_cifar10():
+def cifar10():
     return '/home/jovyan/work/DataLocal-w/ffcv-cifar10/'
+
+def pycortex_db_NSD():
+    return '/home/jovyan/work/DataLocal-w/pycortex_db_NSD'
     
