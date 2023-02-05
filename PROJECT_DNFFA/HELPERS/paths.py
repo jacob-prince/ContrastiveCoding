e90dca06bdca720f6c43945e95c6e30f5ba5b2cd
Jacob Prince <jacob.samuel.prince@gmail.com>
2023-02-05 15:23:57 -0500

    updated gitignore and paths

--- PROJECT_DNFFA/HELPERS/paths.py
+++ PROJECT_DNFFA/HELPERS/paths.py
@@ -1,9 +1,20 @@
+def base():
+    return '/home/jovyan/work' 
+
 def nsd():
-    return '/home/jovyan/work/DataLocal-w/NSD'
+    return f'{base()}/DataLocal-w/NSD'
 
 def cifar10():
-    return '/home/jovyan/work/DataLocal-w/ffcv-cifar10/'
+    return f'{base()}/DataLocal-w/ffcv-cifar10/'
 
 def pycortex_db_NSD():
-    return '/home/jovyan/work/DataLocal-w/pycortex_db_NSD'
-    
+    return f'{base()}//DataLocal-w/pycortex_db_NSD'
+
+def ffcv_imagenet1k_trainset():
+    return f'{base()}/DataLocal-ro/imagenet1k-ffcv/imagenet1k_train_jpg_q100_s256_lmax512_crop_includes_index.ffcv'
+
+def ffcv_imagenet1k_valset():
+    return f'{base()}/DataLocal-ro/imagenet1k-ffcv/imagenet1k_val_jpg_q100_s256_lmax512_crop_includes_index.ffcv'
+
+def training_checkpoint_dir():
+    return f'{base()}/DropboxProjects/DNFFA/PROJECT_DNFFA/EXPERIMENTS_MODELS/checkpoints'
