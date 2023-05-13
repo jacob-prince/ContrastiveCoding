8117f8d4ef7433cca0eed6077e47d3a792f11a34
Jacob Prince <jacob.samuel.prince@gmail.com>
2023-05-12 20:10:33 -0400

    updated environment

--- environment/env_build_from_scratch.sh
+++ environment/env_build_from_scratch.sh
@@ -2,16 +2,22 @@
 
 envname=$1
 
+pip install --upgrade pip
+
 conda deactivate
-conda create --clone ffcv --name $envname
+
+mamba create -y -n $envname python=3.9 cupy=11.6 pkg-config compilers libjpeg-turbo opencv pytorch-gpu cudatoolkit=11.7 torchvision torchaudio pytorch-cuda=11.7 numba -c pytorch -c nvidia -c conda-forge
 
 conda activate $envname
 
-# install pycortex and sklearn
-pip install pycortex==1.2.6
+# install ffcv
+pip install ffcv
+
+# install pycortex
+pip install pycortex
 pip install numpy==1.23.5
+pip install scipy==1.10.1
 pip install numba==0.56.4
-pip install scipy==1.10.0
 
 # packages for testing environment in jupyterlab
 mamba install ipykernel ipython jupyterlab ipywidgets
@@ -34,11 +40,20 @@ mamba install pycocotools
 pip install graphviz
 pip install git+https://github.com/johnmarktaylor91/torchlens
 
+# other packages for analyses
+mamba install torchmetrics seaborn nibabel h5py
+
 # install this project package
 pip install --user -e ../
 
-# test imports
-python3 env_test_imports.py
-
 # set directory of the pycortex database
 python3 pycortex_database_setup.py
+
+# install circuit pruning tools
+mamba install umap-learn=0.5.3 dash=2.7.1 jupyter-dash=0.4.2 pyarrow=10.0.1
+pip install lucent==0.1.0 kornia==0.4.1 kaleido==0.2.1
+
+pip install --user -e /home/jovyan/work/DropboxSandbox/circuit_pruner_iccv2023/circuit_pruner_code
+
+# test imports
+python3 env_test_imports.py
--- environment/env_test_imports.py
+++ environment/env_test_imports.py
@@ -1,4 +1,19 @@
 import numpy as np
+import torch
+import torchvision
+import ffcv
+from ffcv.fields import IntField, RGBImageField
+from ffcv.fields.decoders import IntDecoder, SimpleRGBImageDecoder
+from ffcv.loader import Loader, OrderOption
+from ffcv.pipeline.operation import Operation
+from ffcv.transforms import RandomHorizontalFlip, Cutout, \
+    RandomTranslate, Convert, ToDevice, ToTensor, ToTorchImage
+from ffcv.transforms.common import Squeeze
+from ffcv.writer import DatasetWriter
+import wandb 
+from pycocotools.coco import COCO
+
+import numpy as np
 import torchvision
 
 from ffcv.fields import IntField, RGBImageField
@@ -9,13 +24,17 @@ from ffcv.transforms import RandomHorizontalFlip, Cutout, \
     RandomTranslate, Convert, ToDevice, ToTensor, ToTorchImage
 from ffcv.transforms.common import Squeeze
 from ffcv.writer import DatasetWriter
-
+import gc
 import torch
 
 x = torch.rand(100)
-x.shape
 
-import cortex
+x.to('cuda:0')
+
+del x 
+
+torch.cuda.empty_cache()
+gc.collect()
 
 import sklearn
 import pandas as pd
@@ -24,8 +43,14 @@ import scipy
 from pycocotools.coco import COCO
 import graphviz
 import torchlens
+import nibabel as nib
+import seaborn as sns
+import h5py
+import sklearn
+import wandb
+import cortex
 
-from PROJECT_DNFFA.ANALYSES import hello
+from jsputils import encoding, lesioning, losses, nnutils, nsdorg, paths, plotting, readout, selectivity, statsmodels, training, validation
 
-hello()
+print('finished successfully.')
 
